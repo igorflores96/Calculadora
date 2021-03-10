d2ffc66a8920a49a6c45a8ea5ee992bd8397ed8f
Telefo <igor.flores96@hotmail.com>
2021-03-09 21:53:35 -0300

    Inclusão do teste no origem

--- Calculadora/Primeiro projeto/Origem.cpp
+++ Calculadora/Primeiro projeto/Origem.cpp
@@ -8,6 +8,7 @@ int main()
 	Calculadora	calculadora;
 
 	cout << "A soma dos valores foi de: " << calculadora.somar(5.0, 5.0) << endl;
+	cout << "A subtração dos valores foi de: " << calculadora.subtrair(5.0, 5.0) << endl;
 
 	return 0;
 
